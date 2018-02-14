8e7ef25a0e7b8ad48a814395852ebc992b95008a
Mark Allen Evans <president@worldvaporexpo.com>
2018-02-13 19:45:34 -0500

    Update YourMoneroRequests.cpp

--- src/YourMoneroRequests.cpp
+++ src/YourMoneroRequests.cpp
@@ -1484,7 +1484,7 @@ YourMoneroRequests::get_version(const shared_ptr< Session > session, const Bytes
         {"last_git_commit_hash", string {GIT_COMMIT_HASH}},
         {"last_git_commit_date", string {GIT_COMMIT_DATETIME}},
         {"git_branch_name"     , string {GIT_BRANCH_NAME}},
-        {"monero_version_full" , string {HAVEN_VERSION_FULL}},
+        {"monero_version_full" , string {MONERO_VERSION_FULL}},
         {"api"                 , OPENMONERO_RPC_VERSION},
         {"testnet"             , CurrentBlockchainStatus::testnet},
         {"blockchain_height"   , get_current_blockchain_height()}
